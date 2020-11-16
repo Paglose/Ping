ba9e7e52842ba021a941511ed80579b0af90152f
Pagwin <Pagwin.Fedora@protonmail.com>
2020-11-15 22:12:58 -0500

    some fixes

--- src/ping.ts
+++ src/ping.ts
@@ -2,5 +2,5 @@ import * as discord from "discord.js";
 
 const DISCORD_EPOCH:number = 1420070400000;
 export function onMessage(message:string, eventData, messageObj:discord.Message){
-    messageObj.reply(`${new Date().getTime() - messageObj.createdTimestamp - DISCORD_EPOCH} ms ping`);
+    if(!messageObj.author.bot) messageObj.reply(`${new Date().getTime() - messageObj.createdTimestamp} ms ping`);
 }
