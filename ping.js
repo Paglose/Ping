dc969f760938eaee5b4c1fc3a72f38e451d5898e
Pagwin <Pagwin.Fedora@protonmail.com>
2020-11-15 22:14:20 -0500

    rolled out a fix

--- ping.js
+++ ping.js
@@ -2,6 +2,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.onMessage = void 0;
 const DISCORD_EPOCH = 1420070400000;
 function onMessage(message, eventData, messageObj) {
-    messageObj.reply(`${new Date().getTime() - messageObj.createdTimestamp - DISCORD_EPOCH} ms ping`);
+    if (!messageObj.author.bot)
+        messageObj.reply(`${new Date().getTime() - messageObj.createdTimestamp} ms ping`);
 }
 exports.onMessage = onMessage;
