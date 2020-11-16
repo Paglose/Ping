6152fb591ae5c77c8f0c2408dcf89c2a76ac7116
Pagwin <Pagwin.Fedora@protonmail.com>
2020-11-15 22:18:40 -0500

    made it only respond to ping command

--- src/ping.ts
+++ src/ping.ts
@@ -2,5 +2,5 @@ import * as discord from "discord.js";
 
 const DISCORD_EPOCH:number = 1420070400000;
 export function onMessage(message:string, eventData, messageObj:discord.Message){
-    if(!messageObj.author.bot) messageObj.reply(`${new Date().getTime() - messageObj.createdTimestamp} ms ping`);
+    if(!messageObj.author.bot && message === "p!ping") messageObj.reply(`${new Date().getTime() - messageObj.createdTimestamp} ms ping`);
 }
