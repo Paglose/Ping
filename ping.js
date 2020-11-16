e382b0f4f2819cdb07cf5bd19340ca5467c586b0
Pagwin <Pagwin.Fedora@protonmail.com>
2020-11-15 22:19:16 -0500

    updated to require ping command

--- ping.js
+++ ping.js
@@ -2,7 +2,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.onMessage = void 0;
 const DISCORD_EPOCH = 1420070400000;
 function onMessage(message, eventData, messageObj) {
-    if (!messageObj.author.bot)
+    if (!messageObj.author.bot && message === "p!ping")
         messageObj.reply(`${new Date().getTime() - messageObj.createdTimestamp} ms ping`);
 }
 exports.onMessage = onMessage;
